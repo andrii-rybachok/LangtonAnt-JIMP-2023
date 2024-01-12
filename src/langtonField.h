917a476f6bc65682561d5b168b22ca9d5f9ba2da
andrii-rybachok <80616761+andrii-rybachok@users.noreply.github.com>
2024-01-12 10:00:01 +0100

    repaired symbols

--- src/langtonField.h
+++ src/langtonField.h
@@ -4,16 +4,16 @@
 #include <stdio.h>
 #include <wchar.h>
 
-#define ARROW_NORTH_WHITE '^'
-#define ARROW_NORTH_BLACK 'N'
-#define ARROW_EAST_WHITE '>'
-#define ARROW_EAST_BLACK 'E'
-#define ARROW_SOUTH_WHITE 'v'
-#define ARROW_SOUTH_BLACK 'S'
-#define ARROW_WEST_WHITE '<'
-#define ARROW_WEST_BLACK 'W'
-#define SQUARE_WHITE L' '
-#define SQUARE_BLACK L'█'
+#define ARROW_NORTH_WHITE "^"
+#define ARROW_NORTH_BLACK "^^"
+#define ARROW_EAST_WHITE "<"
+#define ARROW_EAST_BLACK "<<"
+#define ARROW_SOUTH_WHITE "v"
+#define ARROW_SOUTH_BLACK "v"
+#define ARROW_WEST_WHITE ">"
+#define ARROW_WEST_BLACK ">>"
+#define SQUARE_WHITE 'W'
+#define SQUARE_BLACK 'B'
 
 typedef struct {
     int** field;
